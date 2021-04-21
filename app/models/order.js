1fa988232549f2598c457d63db5184f0f8c66405
Naman Jain <namanb139j@gmail.com>
2021-04-22 01:51:11 +0530

    test

--- app/models/order.js
+++ app/models/order.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const User = require('./user')
 const Schema = mongoose.Schema
 
-<<<<<<< HEAD
 const orderSchema=new Schema({
     customerId:{type:mongoose.Schema.Types.ObjectId,
     ref:User,
@@ -18,27 +17,6 @@ const orderSchema=new Schema({
    
    
 },{timestamps:true})
-=======
-const orderSchema = new Schema(
-  {
-    customerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: User,
-      required: true
-    },
-    FarmerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: User,
-      required: true
-    },
-    items: { type: Object, required: true },
-    phone: { type: String, required: true },
-    address: { type: String, required: true },
-    status: { type: String, default: 'Accepted' }
-  },
-  { timestamps: true }
-)
->>>>>>> 93584491da4097a23ba3dd690d0c842629c010ed
 
 const order = mongoose.model('Order', orderSchema)
 module.exports = order
